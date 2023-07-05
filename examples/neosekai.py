bd8e7a6fb5d3a418e0530fdf47a06cbb76e39d2b
Felix Melo <felix.melo.aviles@hotmail.com>
2023-07-05 02:48:09 -0400

    feat(examples): add neosekai example template

---
+++ examples/neosekai.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('..')
+
+from epub_scrapper import EpubScrapper #pylint: disable=c0413
+
+def sanitize_content(content):
+    for i in content.find_all('div', style='text-align: center;'):
+        if i.find('img') and i.find('img').get('src')[0:4] == 'epub':
+            i.replace_with(i.find('img'))
+        else:
+            i.decompose()
+    for i in content.find_all('span'):
+        i.decompose()
+
+def chapter_title(soup):
+    return soup.title.string[soup.title.string.find('Chapter'):soup.title.string.find('NeoSekai')-3]
+
+def next_chapter(soup):
+    return soup.find("div", class_='nav-next').find('a', href=True)['href']
+
+args = {
+    'title': 'some title',
+    'starting_chapter_link': 'https://www.neosekaitranslations.com/novel/since-ive-entered-the-world-of-romantic-comedy-manga-ill-do-my-best-to-make-the-heroine-who-doesnt-stick-with-the-hero-happy/chapter-1-reincarnating-as-a-manga-character/',
+    'starting_chapter_number': 1,
+    'ending_chapter_number': 2,
+    'file_name': 'some file name',
+    'content_class': ['reading-content', 'text-left'],
+    'chapter_title_function': chapter_title,
+    'sanitizing_function': sanitize_content,
+    'language': 'en',
+    'next_chapter_function': next_chapter
+}
+
+EpubScrapper(**args)
